e96f0dde1517e67e20309b2941b1c6618ad9b9f5
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-09-18 13:02:12 +0600

    Axios added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import DaisyNav from "./components/DaisyNav/DaisyNav";
+import LineChart from "./components/LineChart/LineChart";
 import Navbar from "./components/Navbar/Navbar";
+import Phones from "./components/Phones/Phones";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <Navbar></Navbar>
       <h1 className="text-7xl">Price Options</h1>
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
+      <Phones></Phones>
     </>
   );
 }
---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,89 @@
+import { LineChart as LChart, Line, XAxis, YAxis } from "recharts";
+const LineChart = () => {
+  const math_marks = [
+    {
+      id: 1,
+      name: "John",
+      math: 65,
+      physics: 72,
+      chemistry: 88,
+    },
+    {
+      id: 2,
+      name: "Alice",
+      math: 78,
+      physics: 94,
+      chemistry: 72,
+    },
+    {
+      id: 3,
+      name: "Bob",
+      math: 92,
+      physics: 85,
+      chemistry: 78,
+    },
+    {
+      id: 4,
+      name: "Emily",
+      math: 88,
+      physics: 90,
+      chemistry: 87,
+    },
+    {
+      id: 5,
+      name: "David",
+      math: 76,
+      physics: 82,
+      chemistry: 85,
+    },
+    {
+      id: 6,
+      name: "Olivia",
+      math: 95,
+      physics: 93,
+      chemistry: 91,
+    },
+    {
+      id: 7,
+      name: "Michael",
+      math: 89,
+      physics: 79,
+      chemistry: 88,
+    },
+    {
+      id: 8,
+      name: "Sophia",
+      math: 70,
+      physics: 91,
+      chemistry: 94,
+    },
+    {
+      id: 9,
+      name: "William",
+      math: 81,
+      physics: 84,
+      chemistry: 80,
+    },
+    {
+      id: 10,
+      name: "Ella",
+      math: 87,
+      physics: 89,
+      chemistry: 86,
+    },
+  ];
+
+  return (
+    <div>
+      <LChart width={500} height={400} data={math_marks}>
+        <XAxis dataKey={"name"}></XAxis>
+        <YAxis></YAxis>
+        <Line dataKey="math" stroke="blue"></Line>
+        <Line dataKey="physics" stroke="green"></Line>
+        <Line dataKey="chemistry" stroke="yellow"></Line>
+      </LChart>
+    </div>
+  );
+};
+
+export default LineChart;
